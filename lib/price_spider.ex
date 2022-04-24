acc0a8635e1117aa49fabb63e81bcab3ce0e5d69
Allan MacGregor <amacgregor@allanmacgregor.com>
2022-04-24 10:25:29 -0300

    Initial commit with base code

---
+++ lib/price_spider.ex
@@ -0,0 +1,18 @@
+defmodule PriceSpider do
+  @moduledoc """
+  Documentation for `PriceSpider`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PriceSpider.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/price_spider_test.exs
@@ -0,0 +1,8 @@
+defmodule PriceSpiderTest do
+  use ExUnit.Case
+  doctest PriceSpider
+
+  test "greets the world" do
+    assert PriceSpider.hello() == :world
+  end
+end

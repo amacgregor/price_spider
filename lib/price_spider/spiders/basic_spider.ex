ee8442c18274399dbb36775b4c756505782b0d2a
Allan MacGregor <amacgregor@allanmacgregor.com>
2022-04-24 11:46:08 -0300

    Add libraries and basic crawler

---
+++ lib/price_spider/spiders/basic_spider.ex
@@ -0,0 +1,23 @@
+defmodule PriceSpider.BasicSpider do
+  use Crawly.Spider
+  @impl Crawly.Spider
+  def base_url do
+    "http://www.amazon.com"
+  end
+
+  @impl Crawly.Spider
+  def init() do
+    [
+      start_urls: [
+        "https://www.amazon.com/ZOTAC-Graphics-IceStorm-Advanced-ZT-A30820J-10PLHR/dp/B09PZM76MG/ref=sr_1_3?crid=1CLZE45WJ15HH&keywords=3080+graphics+card&qid=1650808965&sprefix=3080+%2Caps%2C116&sr=8-3",
+        "https://www.amazon.com/GIGABYTE-Graphics-WINDFORCE-GV-N3080GAMING-OC-12GD/dp/B09QDWGNPG/ref=sr_1_4?crid=1CLZE45WJ15HH&keywords=3080+graphics+card&qid=1650808965&sprefix=3080+%2Caps%2C116&sr=8-4",
+        "https://www.amazon.com/ZOTAC-Graphics-IceStorm-Advanced-ZT-A30800J-10PLHR/dp/B099ZCG8T5/ref=sr_1_5?crid=1CLZE45WJ15HH&keywords=3080+graphics+card&qid=1650808965&sprefix=3080+%2Caps%2C116&sr=8-5"
+      ]
+    ]
+  end
+
+  @impl Crawly.Spider
+  def parse_item(_response) do
+    %Crawly.ParsedItem{:items => [], :requests => []}
+  end
+end
--- mix.exs
+++ mix.exs
@@ -22,8 +22,8 @@ defmodule PriceSpider.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:crawly, "~> 0.13.0"},
+      {:floki, "~> 0.26.0"}
     ]
   end
 end
